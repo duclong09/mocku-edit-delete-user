d8194250d5a6b4f015bfbbff11f48158874be36f
duclonggitnore <duclongsew@gmail.com>
2020-11-23 20:02:22 +0700

    mockup-user

---
+++ src/components/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About </h1>
+       
+          <p>
+            Who we are Helping developers and technologists write the script of
+            the future. Our public platform serves 100 million people every
+            month, making it one of the 50 most popular websites in the world.
+            Our asynchronous knowledge management and collaboration offering
+            Stack Overflow for Teams, is transforming how people work.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default About;
